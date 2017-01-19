8ea52a3a2b7b225c111e6ec9cb34d495f982ab5d
ginggonzalez03 <ginggonzalez3@gmail.com>
2017-01-18 23:40:05 -0800

    feat: Added model to contain the data
    Model is added as part of the MVC(Model View Controller) architecture.

---
+++ framework/js/main.js
@@ -0,0 +1,54 @@
+/* Model */
+var model = {
+	my_info: {
+		name: "Ging Gonzalez",
+		title: "Full Stack Web Developer",
+		bio: "The only thing different...",
+		profile_pic: null,
+		links: {
+			github: "https://github.com/gggonzalez03",
+			linkedin: "https://www.linkedin.com/in/ging-gonzalez",
+			resume: null,
+			twitter: "https://www.linkedin.com/in/ging-gonzalez",
+			facebook: null
+		},
+		projects: [
+			{
+				name: "Multi-user Blog Website",
+				screenshot: null,
+				description: null,
+				github_repo_link: null,
+				technologies_used: [
+					null
+				]
+			},
+			{
+				name: "Multi-user Blog Website",
+				screenshot: null,
+				description: null,
+				github_repo_link: null,
+				technologies_used: [
+					null
+				]
+			},
+			{
+				name: "Swiss Tournament Facilitator",
+				screenshot: null,
+				description: null,
+				github_repo_link: null,
+				technologies_used: [
+					null
+				]
+			},
+			{
+				name: "Neighborhood Map",
+				screenshot: null,
+				description: null,
+				github_repo_link: null,
+				technologies_used: [
+					null
+				]
+			},
+		]
+	}
+};
