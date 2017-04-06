34fb9c6338c9874a527d9b0aea5710d7e96b5867
ginggonzalez03 <ginggonzalez3@gmail.com>
2017-04-05 19:41:54 -0700

    style: Altered project cards' design
    The cards' design is altered for mobile version of the website. However, the
    desktop version or for other bigger screens, the design is broken.

--- framework/js/model.js
+++ framework/js/model.js
@@ -42,7 +42,7 @@ var model = {
 		projects: [
 			{
 				name: "Multi-user Blog Website",
-				screenshot: "http://placekitten.com/g/600/300",
+				screenshot: "http://placehold.it/350x150",
 				description: "Multi-user web application hosted on Google App Engine that enables\
 				 users to post, delete, comment, and like blog posts. Built with secure login functionality\
 				 using password hashes and salt preventing brute force attacks.",
@@ -55,7 +55,7 @@ var model = {
 			},
 			{
 				name: "Restaurant Item Catalog",
-				screenshot: "http://placekitten.com/g/600/300",
+				screenshot: "http://placehold.it/350x150",
 				description: "Content management system that is engineered with Flask framework in\
 				 Python and advanced user authentication using OAuth2 with all its data stored in a\
 				 PostgreSQL database",
@@ -71,7 +71,7 @@ var model = {
 			},
 			{
 				name: "Swiss Tournament Facilitator",
-				screenshot: "http://placekitten.com/g/600/300",
+				screenshot: "http://placehold.it/350x150",
 				description: "Python application that ficilitates game tournaments the swiss way.\
 				 Built with a relational database using PostgreSQL.",
 				github_repo_link: "https://github.com/gggonzalez03/fullstack-nanodegree-vm",
@@ -83,7 +83,7 @@ var model = {
 			},
 			{
 				name: "Neighborhood Map",
-				screenshot: "http://placekitten.com/g/600/300",
+				screenshot: "http://placehold.it/350x150",
 				description: "A single-page web application that lets users discover new places\
 				 and learn more about them using an interactive map and UI built using Knockout JS,\
 				 Google Maps API, and Foursquare API.",
